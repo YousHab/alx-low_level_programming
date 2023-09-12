372ce6b4ea8c75deedb980c9c0dd7edae63c5419
youssef <haboulyoussef14@gmail.com>
2023-09-12 08:38:51 -0700

    comm

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 	char c = 'a';
 	int i = 0;
 
-	for (i = 0; i < 10; i++)
+	while (i < 10)
 	{
 		while (c <= 'z')
 		{
@@ -20,5 +20,6 @@ void print_alphabet_x10(void)
 			c++;
 		}
 		_putchar('\n');
+		i++;
 	}
 }
