bddfacaeb41d2ca5850390b2b56a1e4ff452d031
youssef <haboulyoussef14@gmail.com>
2023-09-14 11:22:03 -0700

    m

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,25 +10,22 @@
 
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if (size == 0)
 		_putchar('\n');
 	else
-		j = 0;
-		while (size)
+		for (i = 0; i < size; i++)
 		{
-			for (i = 0; i < size; i++)
+			for (j = 1; j <= (size - i - 1); j++)
 			{
 				_putchar(' ');
-				j++;
 			}
-			if (j == (size - i))
-				for (j = size - i; j <= size; j++)
-				{
+
+			for (k = 0; k <= (size - j); k++)
+			{
 				_putchar('#');
-				}
-			_putchar('\n');
-			size--;
+			}
+		_putchar('\n');
 		}
 }
