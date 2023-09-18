64df7fc7e323cb46a913d017add4eda18c7a7ad4
YousHab <haboulyousse14@gmail.com>
2023-09-18 19:58:39 +0100

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,14 @@
 void print_array(int *a, int n)
 {
 	int i;
-
-	for (i = 0; i < n - 1; i++)
+	
+	if (n != 0)
 	{
-		printf("%d, ", a[i]);
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d, ", a[i]);
+		}
+		printf("%d", a[n - 1]);
 	}
-	printf("%d\n", a[n - 1]);
-}
+	printf('\n');
+/bin/bash: line 1: q: command not found
