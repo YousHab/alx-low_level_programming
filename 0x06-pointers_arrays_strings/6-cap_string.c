e8b28ff163cb9025db6851bda3a226cf8718fbe6
YousHab <haboulyousse14@gmail.com>
2023-09-21 00:21:01 +0100

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,23 +12,26 @@ char *cap_string(char *s)
 {
 	int i, j;
 
-	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+	char separators[13] = {' ', '\t', '\n', ',', ';', '.',
 		'!', '?', '"', '(', ')', '{', '}'};
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 32;
-		for (j = 0; j < 13; j++)
+			s[i] = s[i] - 32;
+
+		while (j < 13)
 		{
-			if (s[i] == spe[j])
+			j = 0;
+			if (s[i] == separators[j])
 			{
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				{
-					s[i + 1] -= 32;
-				}
+					s[i + 1] = s[i + 1] - 32;
 			}
+			j++;
 		}
-	}
+		i++;
+	}	
 	return (s);
 }
