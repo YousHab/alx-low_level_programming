335f245734e98cc29278c55bcb96effec11651d2
YousHab <haboulyousse14@gmail.com>
2023-09-20 22:35:55 +0100

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,7 +10,7 @@
 
 char *leet(char *s)
 {
-	char a[5] = {'a', 'b', 'o', 't', 'l'};
+	char a[5] = {'a', 'e', 'o', 't', 'l'};
 	char b[5] = {'A', 'E', 'O', 'T', 'L'};
 	char c[5] = {'4', '3', '0', '7', '1'};
 	int i = 0;
@@ -18,6 +18,7 @@ char *leet(char *s)
 
 	while (s[i] != '\0')
 	{
+		j = 0;
 		while (j < 5)
 		{
 			if ((s[i] == a[j]) || (s[i] == b[j]))
