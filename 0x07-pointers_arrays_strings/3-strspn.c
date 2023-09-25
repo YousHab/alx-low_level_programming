20d52c6be920f5c92f25d3e122f4f910031d8f07
YousHab <haboulyousse14@gmail.com>
2023-09-25 13:30:01 +0100

    0x07

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,7 +15,7 @@ unsigned int _strspn(char *s, char *accept)
 	int i = 0;
 	int j;
 
-	while (*(s + i) != '\0')
+	while (*(s + i) != '\0' && *(s + i) != ' ')
 	{
 		j = 0;
 		while (*(accept + j) != '\0')
