76aba86834c7a9c2ace0f79b7feb0c00bfc553d2
YousHab <haboulyousse14@gmail.com>
2023-09-28 16:30:05 +0100

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,9 +7,11 @@
  * Return: The factorial of n.
  */
 
-unsigned int factorial(unsigned int n)
+int factorial(int n)
 {
-	if (n == 0 || n == 1)
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
 		return (1);
 	return (n * factorial(n - 1));
 }
