6ead7e069ee51f071db019c37dba7b22df5c79d8
YousHab <haboulyousse14@gmail.com>
2023-09-28 15:36:58 +0100

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,7 +9,7 @@
 
 void _puts_recursion(char *s)
 {
-	if (s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
@@ -17,4 +17,3 @@ void _puts_recursion(char *s)
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
-	
