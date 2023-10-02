cf17734567974e9fe474f7cb09276642b11716ed
YousHab <haboulyousse14@gmail.com>
2023-10-02 13:49:38 +0100

    Task0 - 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints its name, followed by a new line.
+ *
+ * @argc: number of parameters.
+ * @argv: arguments
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A progrme that prints the number of arguments passed into it.
+ *
+ * @argc: number of arguments.
+ * @argv: list of arguments.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+	return (0);
+}
+
