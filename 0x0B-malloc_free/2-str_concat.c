a85451cdad2ef21a44f0dc22facc669686560e47
YousHab <haboulyousse14@gmail.com>
2023-10-04 00:55:50 +0100

    m

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,9 +18,13 @@ char *str_concat(char *s1, char *s2)
 	int i, j, len1, len2;
 	char *p;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL  s2 != NULL)
 	{
-		return (NULL);
+		return (s2);
+	}
+	if (s1 != NULL && s2 == NULL)
+	{
+		return (s1)
 	}
 
 	len1 = strlen(s1);
