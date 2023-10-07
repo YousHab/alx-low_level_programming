666376a479055bf09ee497d3b1810b86fab1c576
YousHab <haboulyousse14@gmail.com>
2023-10-07 15:50:31 +0100

    m

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 /**
  * *_calloc - Function that allocates memory for an array using malloc.
  *
@@ -23,5 +24,38 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	memset(p, 0, size);
+
 	return (p);
+}
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
+}
+int main(void)
+{
+	char *a;
+
+	a = _calloc(98, sizeof(char));
+	strcpy(a, "Best");
+	strcpy(a + 4, " School! :)\n");
+	a[97] = '!';
+	simple_print_buffer(a, 98);
+	free(a);
+	return (0);
 }
