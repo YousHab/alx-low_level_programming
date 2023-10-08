8211fffb32ac7e87ceae9a44defd2893f691c42f
YousHab <haboulyousse14@gmail.com>
2023-10-08 14:59:37 +0100

    m

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,55 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 /**
- * is_positive_integer - fct tha klfsj
- * @str: kfdlkg
+ * main - a program that multiplies two positive numbers.
+ * @argc: The number of arguments.
+ * @argv: the arguments.
  *
- * Return: 1 or 0.
- */
-
-int is_positive_integer(const char *str)
-{
-	while (*str)
-	{
-		if (!isdigit(*str))
-		{
-			return (0);
-		}
-		str++;
-	}
-	return (1);
-}
-/**
- * main - hsglqkn
- * @argc: lgjdmls
- * @argv: ljsdgmds
- *
- * Return: 0 98.
+ * Return: 0.
  */
 
 int main(int argc, char *argv[])
 {
-	char *num1;
-	char *num2;
-	int result;
+	long int num1, num2, result, i, j = 0;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
-	num1 = argv[1];
-	num2 = argv[2];
 
-	if (!(is_positive_integer(num1) && is_positive_integer(num2)))
+	for (i = 1; i < 3; i++)
 	{
-		printf("Error\n");
-		return (98);
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				exit(98);
+			}
+			j++;
+		}
 	}
 
-	result = atoi(num1) * atoi(num2);
-	printf("%d\n", result);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
+	result = num1 * num2;
+
+	printf("%ld\n", result);
 	return (0);
 }
