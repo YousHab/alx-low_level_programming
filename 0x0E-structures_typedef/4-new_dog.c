3c94e1cb694494e5372d9abe7a157cec4c3d3b85
YousHab <haboulyousse14@gmail.com>
2023-10-10 23:06:57 +0100

    m

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -28,8 +28,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(name);
 		return (NULL);
 	}
-	new_dog->name = name;
-	new_dog->owner = owner;
+	new_dog->name = strdup(name);
+	new_dog->owner = strdup(owner);
 	new_dog->age = age;
 
 	return (new_dog);
