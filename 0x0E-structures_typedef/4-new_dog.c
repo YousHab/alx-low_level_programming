eb2d387a4422fb1067eed6956b8d431cb5b0fa91
YousHab <haboulyousse14@gmail.com>
2023-10-10 23:04:13 +0100

    m

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,21 +16,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-
+	if (name == NULL)
+	{
+		free(new_dog);
+		free(owner);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free(new_dog);
+		free(name);
+		return (NULL);
+	}
 	new_dog->name = strdup(name);
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-
 	new_dog->owner = strdup(owner);
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
 	new_dog->age = age;
 
 	return (new_dog);
