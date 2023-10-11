a861e57c730ad70e5e33fffc7250197cd2e60fad
YousHab <haboulyousse14@gmail.com>
2023-10-11 20:16:42 +0100

    m

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,7 @@
  * Return: 0.
  */
 
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
 	int n1, n2, result;
 	int (*op)(int, int);
@@ -19,9 +19,22 @@ int main(int argc, int *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+
+        n1 = atoi(argv[1]);
+        n2 = atoi(argv[3]);
+
+	op = get_op_func(argv[2]);
+	if (!op)
+	{	
+		printf("Error\n");
+		exit(99);
+	}
 	
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[2]);
+	if ((*argv[2] == '/' || *argv[2] == '%') & (n2 == 0))
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	result = op(n1, n2);
 
