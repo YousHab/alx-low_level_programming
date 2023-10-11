ae7fe65f99216449c8afdd424d3e07626e1d708d
YousHab <haboulyousse14@gmail.com>
2023-10-11 20:19:39 +0100

    m

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,16 +20,16 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-        n1 = atoi(argv[1]);
-        n2 = atoi(argv[3]);
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
 
 	op = get_op_func(argv[2]);
 	if (!op)
-	{	
+	{
 		printf("Error\n");
 		exit(99);
 	}
-	
+
 	if ((*argv[2] == '/' || *argv[2] == '%') & (n2 == 0))
 	{
 		printf("Error\n");
