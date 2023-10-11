68efe765374303738000002f6d79864a09142b49
YousHab <haboulyousse14@gmail.com>
2023-10-11 16:20:29 +0100

    m

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,10 +13,11 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i = 0;
+	unsigned int i = 0;
 
 	if (!array || !action)
 		return;
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
