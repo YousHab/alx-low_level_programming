e4426b815c0091d57ecc4e93af4de236a1deb500
YousHab <haboulyousse14@gmail.com>
2023-10-19 22:47:36 +0100

    m

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * free_list - a function that frees a list.
+ * @head: address of the head.
+ *
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+	list_t *next_node;
+
+	if (head == NULL)
+		return;
+
+	temp = head;
+	while (node != NULL)
+	{
+		next_node = temp->next;
+		free(node->str);
+		free(temp);
+		node = next_node;
+	}
+}
