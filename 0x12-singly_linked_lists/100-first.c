6772c4a7546b61d05544ca7d1d1242d389bf010c
YousHab <haboulyousse14@gmail.com>
2023-10-19 23:05:17 +0100

    m

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void _first(void) __attribute__((first))
+/**
+ * _first - function that print a string before the the execution
+ * of the main function
+ */
+
+void _first(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
