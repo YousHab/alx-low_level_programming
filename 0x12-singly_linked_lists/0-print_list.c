1fbf5e488e7a2ce17bd5fceaa94d8fae02ac2890
YousHab <haboulyousse14@gmail.com>
2023-10-19 21:05:47 +0100

    m

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,24 @@
 #include "lists.h"
 /**
+ * _strlen - claculate length of a string
+ * @s: a ponter to the string
+ * Retrun: the length.
+ */
+ 
+
+int _strlen(char *s)
+{
+	int count = 0;
+	if (s == NULL)
+		return (0);
+	while(*s++)
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
  * print_list - a function that prints all the elements of a list_t list.
  *
  * @h: the list.
@@ -13,7 +32,7 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%ld] %s\n", strlen(h->str), h->str ? h->str : "(nil)");
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 		h = h->next;
 		count++;
 	}
