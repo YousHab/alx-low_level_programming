c547d8d5dfef4f0f40540ecd82fc92d81c022820
YousHab <haboulyousse14@gmail.com>
2023-10-23 19:46:07 +0100

    m

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,31 @@
 #include "lists.h"
 /**
+ * listint_len - prints the number of elements in a linked listint_t list
+ *
+ * @h: a pointer to the head of the list.
+ *
+ * Return: the length of the list.
+ */
+
+size_t lengthlist(listint_t *h)
+{
+	int count = 0;
+	listint_t *ptr = NULL;
+
+	if (!h)
+		return (0);
+	ptr = h;
+	while (ptr)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
+}
+
+
+
+/**
  * get_nodeint_at_index - returns the nth node of a listint_t linked list.
  *
  * @head: the head.
@@ -12,8 +38,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *p = NULL;
 	unsigned int i = 0;
+	size_t n;
 
 	if (head == NULL)
+		return (NULL);
+
+	n = lengthlist(head);
+	if (index > n || index < 0)
 		return (NULL);
 
 	p = head;
