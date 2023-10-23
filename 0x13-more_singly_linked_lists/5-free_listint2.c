2a3590d560b0d4e09a69c5c44fac563fa9cd8bd2
YousHab <haboulyousse14@gmail.com>
2023-10-23 18:45:14 +0100

    m

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list
+ *
+ * @head: head.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr = NULL;
+
+	while(*head)
+	{
+		ptr = *head;
+		*head = ptr->next;
+		free(ptr);
+	}
+}
