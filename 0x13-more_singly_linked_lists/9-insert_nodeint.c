917a025a86873686e5ca4a16d41dca4e8cb6889d
YousHab <haboulyousse14@gmail.com>
2023-10-23 20:58:24 +0100

    m

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,11 +13,11 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node = NULL;
-	listint_t *ptr = NULL;
+	listint_t *ptr = *head;
 	unsigned int i = 0;
 	unsigned int len = 0;
 
-	if (*head == NULL && idx != 0)
+	if (*head == NULL && idx > 0)
 		return (NULL);
 
 	len = lengthlist(*head);
@@ -32,12 +32,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
-	for (i = 0; i < idx; i++)
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	for (i = 0; i < idx - 1; i++)
 	{
 		ptr = ptr->next;
 	}
+
 	new_node->next = ptr->next;
-	new_node = ptr;
+	ptr->next = new_node;
 
 	return (new_node);
 }
