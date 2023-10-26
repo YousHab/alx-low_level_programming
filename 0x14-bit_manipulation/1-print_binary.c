1c77b7315381705571d60bf5e4e56d23b036eab7
YousHab <haboulyousse14@gmail.com>
2023-10-26 12:19:30 +0100

    m

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,19 +7,7 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int x = 1;
-
-	if (n == 0)
-		_putchar('0');
-	if (n == 1)
-		_putchar('1');
-	while (x)
-	{
-		if (x & n)
-			_putchar('1');
-		else
-			_putchar('0');
-		x = x << 1;
-	}
-	_putchar('\n');
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + 48);
 }
