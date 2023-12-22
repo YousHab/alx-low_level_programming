ba6ef2a56c3687c085410c59856ebe4e64537925
YousHab <haboulyousse14@gmail.com>
2023-12-22 15:48:54 +0100

    m

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *new_hash_table = NULL;
+	hash_table_t *new_hash_table;
 
 	if (size == 0)
 		return (NULL);
@@ -23,6 +23,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (!new_hash_table->array)
+		free(new_hash_table);
 		return (NULL);
 
 	for (i = 0; i < size; i++)
---
+++ 0x1A-hash_tables/1-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(void)
+{
+	hash_table_t *ht;
+
+        ht = hash_table_create(1024);
+	printf("%p\n", (void *)ht);
+	return (EXIT_SUCCESS);
+}
