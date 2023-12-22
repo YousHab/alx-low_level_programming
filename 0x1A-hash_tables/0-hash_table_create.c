cb5bb8e2d30243401f66953490ba392c706d4c8b
YousHab <haboulyousse14@gmail.com>
2023-12-22 15:38:38 +0100

    m

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size == 0)
 		return (NULL);
 
-	new_hash_table = malloc(sizeof(hasht_table_t));
+	new_hash_table = malloc(sizeof(hash_table_t));
 	if (!new_hash_table)
 		return (NULL);
 
