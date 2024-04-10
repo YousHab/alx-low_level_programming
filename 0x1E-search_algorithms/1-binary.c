b46e87055ca6646cd7ffcf8ec596d7788d95f6f1
YousHab <haboulyousse14@gmail.com>
2024-04-10 11:40:37 +0000

    m

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,50 +1,39 @@
 #include "search_algos.h"
 
 /**
- * inary_search - searches for a value in a sorted array of integers
+ * binary_search - searches for a value in a sorted array of integers
  * @array: the array
  * @size: the size
  * @value: the value
  *
  * Return: the index or -1 if not found
  */
+
 int binary_search(int *array, size_t size, int value)
 {
-	size_t mid = size / 2;
-	int result = 0;
-	size_t j = 0;
+	size_t i = 0;
+	int *a_cp = array;
 
-	if (!array || size == 0)
+	if (!array)
 		return (-1);
 
-	printf("Searching in array: ");
-	for (j = 0; j < size; j++)
+	while (size)
 	{
-		if (j == size - 1)
-		{
-			printf("%d\n", array[j]);
-		}
-		else
-		{
-			printf("%d, ", array[j]);
-		}
-	}
+		for (i = 0, printf("Searching in array: "); i < size; i++)
+			printf("%d%s", a_cp[i], i + 1 == size ? "\n" : ", ");
 
-	if (array[mid] == value)
-	{
-		return (mid);
-	}
-	else if (array[mid] < value)
-	{
-		result = binary_search(array + mid + 1, size - mid - 1, value);
-		if (result == -1)
-		{
-			return (-1);
-		}
+		i = (size - 1) / 2;
+		if (a_cp[i] == value)
+			return ((a_cp - array) + i);
+		else if (a_cp[i] > value)
+			size = i;
 		else
 		{
-			return (mid + 1 + result);
+			a_cp += (i + 1);
+			size -= (i + 1);
 		}
+
 	}
-	return (binary_search(array, mid, value));
+	return (-1);
 }
+
